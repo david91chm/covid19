add4aaba4bc0903f5d275449e7e91d9b2cd06a65
efrain <efrainpaxi@gmail.com>
2020-03-25 23:35:10 -0400

    efrax
    efrax

--- views/site/anestesia.php
+++ views/site/anestesia.php
@@ -21,10 +21,6 @@
 			<li>Sistema de conexión de sonda</li>
 			<li>Jeringa 10ml</li>
 			<li>Conexión anillada</li>
-			<li>Conexión anillada</li>
-			<li>Conexión anillada</li>
-			<li>Conexión anillada</li>
-			<li>Conexión anillada</li>
 		</ul>
 	</div>
 </div>
