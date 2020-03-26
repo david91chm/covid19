1cb077c983cf2606786e79548e0d39965020197e
efrain <efrainpaxi@gmail.com>
2020-03-25 23:37:50 -0400

    efraz
    efraz

--- views/site/anestesia.php
+++ views/site/anestesia.php
@@ -21,6 +21,9 @@
 			<li>Sistema de conexión de sonda</li>
 			<li>Jeringa 10ml</li>
 			<li>Conexión anillada</li>
+			<li>Conexión anillada</li>
+			<li>Conexión anillada</li>
+			<li>Conexión anillada</li>
 		</ul>
 	</div>
 </div>
