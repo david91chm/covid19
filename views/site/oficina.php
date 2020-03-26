404b9e92a13f353303012c729ff985508a629e26
david91chm <david91chm@gmail.com>
2020-03-25 23:52:45 -0400

    en la oficina

---
+++ views/site/oficina.php
@@ -0,0 +1,11 @@
+<div class="page-header">
+	<h1>En la Oficina</h1>
+</div>
+<button class="btn myButton" type="button">
+	<span class="glyphicon glyphicon-chevron-right"></span> Recomendación de consulta
+</button>
+<div class="collapse" id="collapseExample">
+	<div class="well">
+		<p>Content 1</p>
+	</div>
+</div>
