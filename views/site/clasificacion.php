886089ca6e48d9212e71d65dfd9615948a6b385f
efrain <efrainpaxi@gmail.com>
2020-03-26 10:56:03 -0400

    clasificacion
    clasificacion

--- views/site/clasificacion.php
+++ views/site/clasificacion.php
@@ -7,7 +7,13 @@
 </button>
 <div class="collapse" id="collapseExample">
 	<div class="well">
-		<p>Content 1</p>
+		<p>La señalización se colocará frente a la puerta de cada paciente con:</p>
+		<ul>
+			<li> COVID confirmado (Rojo) COVID +</li>
+			<li> COVID fuerte sospecha (Azul)</li>
+			<li> COVID débil sospecha (Verde)
+			RéaCTVR protocolo CHU Amiens</li>
+		</ul>
 	</div>
 </div>
 
@@ -16,7 +22,26 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<p>Paciente confirmado (rojo) COVID +</p>
+		<p>Tratamiento con aislamiento máximo "Aire, gotitas, contacto"</p>
+
+		<p>Equipo: (ver Procedimiento para vestirse / desvestirse)</p>
+		<ul>
+		
+			<li> Máscara FFP2</li>
+			<li> Tapa o gorra protectora</li>
+			<li> Abrigo azul con mangas largas + delantal blanco</li>
+			<li> Para el cuidado húmedo (Tratamientos de "humectación") como el inodoro: "capa plástica blanca"</li>
+			<li> Gafas protectoras</li>
+			<li> Guantes no estériles</li>
+		</ul>
+		<p>Habitación:</p>
+		<ul>
+			<li> Gestión de residuos con circuito especializado</li>
+			<li> Puertas cerradas</li>
+			<li> Ventilación de la habitación 10 min / d abriendo la ventana, puerta cerrada
+			Protocolo RéaCTVR CHU Amiens</li>
+		</ul>
 	</div>
 </div>
 
@@ -25,7 +50,15 @@
 </button>
 <div class="collapse" id="collapseExample">
 	<div class="well">
-		<p>Content 1</p>
+		<p>Paciente con fuerte sospecha (Azul):</p>
+		<p>Cualquier paciente con una tabla de neumonitis alveolointersticial febril bilateral sin encontrar otra causa:</p>
+		<ul>
+			<li> Se toma una muestra por RT-PCR (hisopo nasofaríngeo y / o profundo)</li>
+			<li> Medidas máximas aplicar siempre (ver COVID +)</li>
+			<li> Realizar una segunda muestra en D 3 (hisopo nasofaríngeo y / o profundo) - Si es positivo -> Se convierte en un paciente confirmado COVID +</li>
+		</ul>
+		<p>- Si es negativo -> Medidas de luz con el uso de una máscara quirúrgico solo considere la transferencia a un área de baja carga viral</p>
+		<p>RéaCTVR Protocol CHU Amiens</p>
 	</div>
 </div>
 
@@ -34,6 +67,13 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<p>Paciente con baja sospecha (verde):</p>
+		<p>Cualquier paciente con signos de descompensación respiratoria hipoxémica o hipercapnica en un contexto febril, sin una imagen típica.</p>
+		<p> Se realiza una muestra viral tomada por RT-PCR en la entrada (hisopo nasofaríngeo y / o profundo)</p>
+		<ul>
+			<li> Si el resultado positivo se confirma al paciente COVID+</li>
+			<li> Si es negativo -> Medidas de luz con el uso de la máscara quirúrgica solo, considere la transferencia a un área de baja carga viral</li>
+		</ul>
+		<p>RéaCTVR CHU Protocolo de Amiens</p>
 	</div>
 </div>
--- views/site/infectologia.php
+++ views/site/infectologia.php
@@ -49,7 +49,32 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		
+		<h3>En hisopo</h3>
+		<ul>
+			<li> RT-PCR COVID 19 si no se conoce el estado
+			(repetir en D3 dependiendo de la clasificación
+			inicial)</li>
+			<li> Virus de la gripe por PCR / virus respiratorios
+			multiplex</li>
+		</ul>
+		<h3>Fibroscopia</h3>
+		<ul>
+			<li>ECBC para análisis estándar + PCR intracelular +
+			cultivo BCYE</li>
+			<li>micología con Pneumocystis PCR +
+			investigación de aspergillus</li>
+		</ul>
+		<h3>Serología del lenguaje. micoplasma</h3>
+
+		<h3>hemocultivos</h3>
+
+		<h3>Orinas</h3>
+
+		<p>Legial Ag y Pneumococcus</p>
+
+		<p>RéaCTVR CHU Protocolo de Amiens</p>
+		
 	</div>
 </div>
 
@@ -58,7 +83,25 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<p>COVID-19 + pacientes hospitalizados con
+		insuficiencia respiratoria L / min)</p>
+
+		<p>Plaqueni10 (hidroxicloroquina): 200mg x2 o 3 / d
+		(control ECG QT space), 10 días + Azitromicina
+		250mg: 2 tabletas el primer día, luego
+		1 tableta / día (5 días)</p>
+
+		<p>- Si no hay disponibilidad de Plaquenil: reemplazar
+		con cloroquina (Nivaquina0): 400mgx2 / d +
+		Azitromicina 250mg: 2 tabletas el primer día,
+		luego 1 tableta / día (5 días)</p>
+
+		<p>En ningún caso son recomendaciones, sino una
+		guía práctica para quienes estén considerando
+		usar estos tratamientos fuera de los ensayos
+		clínicos.</p>
+
+		<p>Fuente: SRLF</p>
 	</div>
 </div>
 
@@ -67,7 +110,31 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<p>pcovtp-19 + pacientes con SPRA (pa021Fi02> 200)</p>
+		<ul>
+			<ul> Plaqueni10 (hidroxicloroquina): 200mg x 3 / d
+			(espacio de control de ECG QT), 10 días</ul>
+
+			<li> Kaletra: los últimos datos chinos publicados
+			(Cao B et al. N Engl J Med, 18 de marzo de 2020)
+			no muestra los beneficios de Kaletra0.</li>
+
+			<li> Darunavir podría ser entonces otra alternativa
+			teórica</li>
+			<li> Darunavir (Prezista0) Solución oral
+			600 mg x 2 / D o tableta 600g X 2 / J</li>
+
+			<li> Terapia antibiótica con cefalosporina 3Gou</li>
+		</ul>
+		<p>Amoxicilina / ac clavulánico + azitromicina
+		(u otro macrólido)</p>
+
+		<p>En ningún caso son recomendaciones, sino una
+		guía práctica para quienes estén considerando
+		utilizar estos tratamientos fuera de ensayos
+		clínicos.</p>
+
+		<p>Fuente: SRLF</p>
 	</div>
 </div>
 
@@ -76,7 +143,40 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<h3>Pacientes con COVID + hospitalizados con SDRA
+		moderado y grave (Pa02 / Fi02 <200)</h3>
+
+		<p>Darunavir - Asociación de ritonavir</p>
+
+		<h3>Si el tratamiento oral</h3>
+		<ul>
+			<li> Norvir • (Ritonavir) comprimido 100 mg:
+			1 comprimido x 2 / D</li>
+			<li> Prezista0 (Darunavir) comprimido 600 mg:
+			1 comprimido x 2 / D</li>
+		</ul>
+		<p>Si el tratamiento con el tubo gástrico</p>
+		
+		<p>- Norvir0 (Ritonavir) 100 mg bolsita diluida:
+		1 bolsita x 21 D</p>
+		<p>- Prezista0 (Darunavir) solución oral: 600 mg x 2 / D</p>
+
+		<p>Solución oral de cloroquina: 400 mg x2 / d
+		(si no está disponible: Plaqueni10 200mgx3TJ, cps
+		triturado, con riesgo de intolerancia digestiva)</p>
+
+		<p>- Solicitud de Remdesivir: 200 mg en dosis de
+		carga Jl, luego 100 mg 1 dosis por día en infusión
+		30-60 min: Duración del tratamiento 10 días,
+		combinada con cloroquina 400mgx2 / d o
+		Plaqueni10. Para
+		el Kaletra</p>
+
+		<p>En ningún caso son recomendaciones, sino una
+		guía práctica para quienes estén considerando
+		usar estos tratamientos fuera de las pruebas</p>
+
+		<p>Fuente: SRLF</p>
 	</div>
 </div>
 
@@ -85,7 +185,39 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<ul>
+			<li> Plasonavir (residual) ritonavir / lopinavir,
+			ritonavir / darunavir la hidroxicloroquina y la
+			cloroquina, remdesivir, son necesarias para
+			confirmar Ia biodisponibilidad</li>
+
+			<li> Principales efectos secundarios de los IP
+			(Kaletra, Norvir + Prezista):</li>
+		</ul>
+		<p>Problemas digestivos, diarrea.</p>
+
+		<p>Riesgo de interacciones farmacológicas +++:</p>
+		<p>-inhibidores del citocromo 450:
+		riesgo de sobredosis de todos los medicamentos
+		asociados metabolizados por Ie
+		citocromo hepático (amiodarona, alfabloqueantes,
+		colchicina, estatinas ...)</p>
+
+		<p>Posibilidad de bradicardia bajo Kaletra,
+		especialmente si los tratamientos de bradicardia
+		asociado (Cuidado con los bloqueadores B,
+		bloqueadores de los canales de calcio; posibilidad
+		de usar lercanidipina, amlodipina)</li>
+
+		<p>Precauciones para cloroquina y OHcloroquina:</p>
+		<p>Extensión de espacio QT; Control de ECG al menos
+		en OJ, JI, J3, J6, J9, J12
+		Cl formal: Atarax, Seroplex, domperidona, ...
+		(+ todas las drogas que alargan la
+		QT), + atención si asociación con macrólidos.
+		Deficiencia de G6PD (hemólisis)</p>
+
+		<p>Fuente: SRLF</p>
 	</div>
 </div>
 
@@ -94,6 +226,62 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<h3>Corticosteroides</h3>
+
+		<p>indicatigng</p>
+
+		<p>Pacientes con COVID-19 + en terapia antiviral:</p>
+
+		<p>- Pa02 / Fi02 <200</p>
+
+		<p>-Shock = uso de noradrenalina o equivalente, y
+		lactato> 2 mmoI / L</p>
+
+		<p>Términos (ver fuente vinculada)</p>
+
+		<p>interferón Y (IMUKIN). la dosis es de 100 ug148 H
+		por vía subcutánea durante 10 días
+		tenga en cuenta, la posibilidad de combinar la
+		terapia con corticosteroides y FNL</p>
+
+		<p>Otro inmunomodulador</p>
+
+		<p>Varias moléculas están bajo investigación en un
+		ensayo clínico y no deberían no se considerará
+		fuera del ensayo clínico.</p>
+
+		<p>La siguiente lista no es exhaustiva: IFN beta
+		inhalado, anticuerpos monoclonales dirigidos
+		Voy a - 6, IL-8, complemento, inhibidores beta de
+		TGF. anti-lgG</p>
+
+		<p>Fuente: SRLF</p>
+	</div>
+</div>
+<button class="btn myButton" type="button">
+	<span class="glyphicon glyphicon-chevron-right"></span> Tratamientos asociados
+</button>
+<div class="collapse" id="collapseExample1">
+	<div class="well">
+		<p>Debido a un carácter muy inflamatorio y la alta
+		incidencia eventos tromboembólicos
+		recomendamos un Anticoagulación curativa con
+		heparina no fraccionada (dilución IOOOUl / mL en
+		PSE), objetivo HNF>02</p>
+
+		<p>NB: la anticoagulación corresponde a una elección
+		de servicio teniendo en cuenta la gran cantidad
+		de eventos tromboembólicos graves</p>
+
+		<p>observados. temprano dependiendo del
+		tolerancia No hay PPI sistemático.</p>
+
+		<p>Además, contraindicaciones para los AINE. y
+		ninguna indicación en el terapia con
+		corticosteroides (al menos en la fase inicial) a
+		menos que el paciente ya esté bajo
+		inmunosupresor</p>
+
+		<p>RéaCTVR CHU Protocolo de Amiens</p>
 	</div>
 </div>
