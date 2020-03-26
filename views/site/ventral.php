642f5e73d936c4c64d82357784e230dbdf76394f
efrain <efrainpaxi@gmail.com>
2020-03-26 01:58:57 -0400

    ventral
    ventral

--- views/site/ventral.php
+++ views/site/ventral.php
@@ -7,7 +7,37 @@
 </button>
 <div class="collapse" id="collapseExample">
 	<div class="well">
-		<p>Content 1</p>
+		<div class="row">
+			<div class="col-md-12">
+				<p>IOT 
+				comprobado en la 
+				fijación de radio ++,  
+				fijación, globo inflado
+				30mmHg </p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>SNG comprobado en 
+				la fijación de radio ++, 
+				fijación </p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>KTC/KTA el vendaje 
+				no se volverá a hacer 
+				en 16hrs</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Catéter Urinario: para 
+				la recolección de 
+				residuos del paciente</p>
+			</div>
+		</div>
+		
 	</div>
 </div>
 
@@ -16,7 +46,35 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<div class="row">
+			<div class="col-md-12">
+				<p>rotección ocular: 
+				suero fisiológico, 
+				vitamina A </p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Aspiraciones/ 
+				sonidos bucales</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Protección de la piel/ 
+				puntos de apoyo: 
+				espuma Duoderm 
+				o Mepilex </p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Retire el material con 
+				riesgo de marcar 
+				el sat frontal</p>
+			</div>
+		</div>
+		<p>Fuente: ayudas cognitivas SFAR - Desglose del SDRA (COVID-19)</p>
 	</div>
 </div>
 
@@ -25,7 +83,30 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Fibroscopio Doppler 
+				Esofágico</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Electrodos limpios</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Sábanas no 
+				desechables</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Cojín para la cabeza 
+				(sin cojín azul)</p>
+			</div>
+		</div>
+		<p>Fuente: ayudas cognitivas SFAR - Desglose del SDRA (COVID-19)</p>
 	</div>
 </div>
 
@@ -34,7 +115,8 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<img src="<?= Yii::$app->request->BaseUrl ?>/images/proteccion.JPG" width="100%" alt="">
+		<p>Fuente: ayudas cognitivas SFAR - Desglose del SDRA (COVID-19)</p>
 	</div>
 </div>
 
@@ -43,7 +125,8 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<img src="<?= Yii::$app->request->BaseUrl ?>/images/Cabeza.JPG" width="100%" alt="">
+		<p>Fuente: ayudas cognitivas SFAR - Desglose del SDRA (COVID-19)</p>
 	</div>
 </div>
 
@@ -52,7 +135,22 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<ul>
+			<li> 1 persona a cargo de la cabeza (médico)
+			  + 3 a 4 PNM distribuidos a cada lado 
+			  del paciente</li>
+			<li> Mantenga las precauciones de aire/ 
+			  contacto/gota (no se muestra en las fotos)</li>
+			<li> Nunca desconecte el patrón del ventilador</li>
+			<li> Proclive 30 de la cama (cabeza más alta 
+			  del pie)</li>
+			<li> Duración de una sesión de DV 16 horas)</li>
+			<li> La alimentación enteral se puede 
+			  mantener si se tolera bien</li>
+			<li> Repita una sesión cada día siempre que 
+			   P / F 150</li>
+		</ul>
+		<p>Fuente: ayudas cognitivas SFAR - Desglose del SDRA (COVID-19)</p>
 	</div>
 </div>
 
@@ -61,6 +159,11 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
+		<h3>Verificación continua de todo lo 
+		relacionado con el paciente 
+		(Distribución de tareas)</h3>
+		<img src="<?= Yii::$app->request->BaseUrl ?>/images/Darse_cuenta.JPG" width="100%" alt="">
+		
 		<p>Content 2</p>
 	</div>
 </div>
