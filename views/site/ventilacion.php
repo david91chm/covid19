7fa9bcad5bc77dcccdacc6baa11507ce7cfcffb4
efrain <efrainpaxi@gmail.com>
2020-03-26 00:42:26 -0400

    ventilacion
    ventilacion

--- views/site/ventilacion.php
+++ views/site/ventilacion.php
@@ -7,7 +7,25 @@
 </button>
 <div class="collapse" id="collapseExample">
 	<div class="well">
-		<p>Content 1</p>
+		<h3>Definición</h3>
+		<ul>
+			<li> Insuficiencia respiratoria aguda que evoluciona
+			por menos de una semana.</li>
+			<li>- Opacidades bilaterales en imágenes torácicas
+			(vidrio esmerilado multifocal bilateral,
+			predominantemente periférico, imágenes de
+			neumonía organizada).</li>
+			<li>- Ausencia de argumentos para un edema
+			hidrostático predominante.</li>
+			<li>- Hipoxemia con una relación
+			Pa02 (mmHg) / F102 (0.21 a 1) <300 para una presión
+			conjunto espiratorio positivo (PEEP) a 5 cm H20
+			Etapas de gravedad (criterios de Berlín).</li>
+		</ul>
+		<h3>Gravedad</h3>
+		<p>3 etapas de gravedad definidas dependiendo de la hipoxemia.</p>
+		<img src="<?= Yii::$app->request->BaseUrl ?>/images/no_demora_la_intubacion.JPG" width="100%" alt="">
+		<p>Fuente: ayudas cognitivas SFAR - Desglose del SDRA (COVID-19)</p>
 	</div>
 </div>
 
@@ -16,7 +34,21 @@
 </button>
 <div class="collapse" id="collapseExample1">
 	<div class="well">
-		<p>Content 2</p>
+		<p>En la ventilación mecánica en fase aguda en modo VAC, tipo ultraprotector con volumen actual a 6 ml / kg de peso ideal (consulte el peso ideal anexo según el tamaño).</p>
+		<ul>
+			<li> Reclutamiento por PEEP set de 6-20 cmH2O.</li>
+			<li> Monitorización de plataforma <28 cmH2O y
+			  presión de conducción <15 cmH2O.</li>
+			<li> FiO2 mínimo establecido para SaO2> 90% SaO2 y
+			  paO2> 60 mmHg.</li>
+			<li> Frecuencia entre 20 - 34 ciclos / min dependiendo
+			  de la gasometría arterial para pH> 7.3.</li>
+			<li> Decúbito ventral en pacientes con P / F <100.</li>
+			<li> Si NO hay indicación: NO prueba y monitoreo de
+			  metahemoglobinemia si el uso es prolongado.</li>
+			<li> Controles de gas.</li>
+		</ul>
+		<p>Fuente: ayudas cognitivas SFAR - Desglose del SDRA (COVID-19)</p>
 	</div>
 </div>
 
